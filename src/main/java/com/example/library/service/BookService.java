d9006bdf2832b05e39667cecbbadc3b1073ad15b
Ganesh Potharaju <ganesh.potharaju1@gmail.com>
2025-06-09 17:15:58 +0530

    Update BookService.java

--- src/main/java/com/example/library/service/BookService.java
+++ src/main/java/com/example/library/service/BookService.java
@@ -1,20 +1,43 @@
-package com.example.library.service;
+package com.example.lms.service;
 
-import com.example.library.model.Book;
-import com.example.library.repository.BookRepository;
+import com.example.lms.entity.Book;
+import com.example.lms.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookService {
-    private final BookRepository bookRepository;
 
-    public BookService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
     }
 
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    public Book getBookById(int id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public void deleteBook(int id) {
+        bookRepository.deleteById(id);
+    }
+
+    public Book updateBook(int id, Book updatedBook) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if (book != null) {
+            book.setTitle(updatedBook.getTitle());
+            book.setAuthor(updatedBook.getAuthor());
+            book.setCategory(updatedBook.getCategory());
+            book.setAvailable(updatedBook.isAvailable());
+            return bookRepository.save(book);
+        }
+        return null;
     }
 }
