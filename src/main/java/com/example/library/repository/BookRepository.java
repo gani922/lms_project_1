32eeb9d60b2bac432089a2edd7b67d721a2154c1
Ganesh Potharaju <ganesh.potharaju1@gmail.com>
2025-06-09 17:17:34 +0530

    Update BookRepository.java

--- src/main/java/com/example/library/repository/BookRepository.java
+++ src/main/java/com/example/library/repository/BookRepository.java
@@ -1,7 +1,16 @@
-package com.example.library.repository;
+package com.example.lms.repository;
 
-import com.example.library.model.Book;
+import com.example.lms.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
+
+package com.example.lms.repository;
+
+import com.example.lms.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
 }
