7643129c761699dfde4bf97de1b42df761249150
Ganesh Potharaju <ganesh.potharaju1@gmail.com>
2025-06-09 17:18:46 +0530

    Create UserRepository.java

---
+++ src/main/java/com/example/library/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.lms.repository;
+
+import com.example.lms.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
+}
+
